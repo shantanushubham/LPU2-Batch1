113554283aae5886c33fcdeea5c7519a9d823c65
Shantanu Shubham <shantanu0707@gmail.com>
2022-07-01 10:17:36 +0530

    Add

---
+++ my-app/src/App.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+
+class App extends Component {
+  render = () => {
+    return (
+      <div>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path={""} element={<HomeScreen />} />
+            <Route path={"/login"} element={<LoginScreen />} />
+            <Route path={"/sign-up"} element={<SignUpScreen />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  };
+}
+
+export default App;
+
+// localhost:3000 -> HomeScreen
+//  /login -> LoginScreen
+//  /sign-up -> SignUpScreen
